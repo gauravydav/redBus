b1cf65ba16aaade2fa30f805e355286ce96580b1
Gaurav Yadav <122369577+gauravydav@users.noreply.github.com>
2023-08-28 19:17:36 +0530

    Update users.js
    Update users.js  data wala api

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const Trip = require('../models/trips')
 const { MongoClient } = require('mongodb');
 // API endpoint to fetch data from the "state_district" collection
-const url = 'mongodb+srv://gauravyadava963998:29071999@cluster0.g55qhrv.mongodb.net/db1?retryWrites=true&w=majority'
 router.get('/data', async (req, res) => {
+  const url = 'mongodb+srv://gauravyadava963998:ndQihJFt8rEKS3N3@cluster0.g55qhrv.mongodb.net/db1?retryWrites=true&w=majority'; // Update this URL with your MongoDB connection details
   const client = new MongoClient(url);
-  let products;
+  
   try {
     await client.connect();
     const db = client.db();
-    products = await db.collection('state_district').find().toArray();
+    const products = await db.collection('state_district').find().toArray();
+    res.json(products);
   } catch (error) {
-    return res.json({message: 'Could not retrieve products.'});
-  };
-  client.close();
-  res.json(products);})
+    console.error(error);
+    res.status(500).json({ message: 'Could not retrieve products.' });
+  } finally {
+    client.close();
+  }
+});
 
 
 router.get('/data1', async (req, res) => {
